cd9eb084233b078138629305c5f5869f1ee7cbe0
Hodor <thapabikash48@gmail.com>
2018-09-22 08:29:53 +0530

    change for articles

--- blog/js/header.js
+++ blog/js/header.js
@@ -1,5 +1,5 @@
 document.write(
 
-  '<!DOCTYPE html><html><head><title>Blog of Programmer | Bikash Thapa</title><link rel="stylesheet" type="text/css" href="css/style.css"><link href="https://fonts.googleapis.com/css?family=Nunito:400,700" rel="stylesheet"></head><body>'
+  '<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width, initial-scale=1"><title>Blog of Programmer | Bikash Thapa</title><link rel="stylesheet" type="text/css" href="css/style.css"><link href="https://fonts.googleapis.com/css?family=Nunito:400,700" rel="stylesheet"></head><body>'
 
   );
