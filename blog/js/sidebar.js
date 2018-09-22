96a4e1388ff7778746e5ed9711a6f2232ba7d568
Hodor <thapabikash48@gmail.com>
2018-09-22 21:27:19 +0530

    Article updated

--- blog/js/sidebar.js
+++ blog/js/sidebar.js
@@ -1,1 +1,1 @@
-document.write(' <div class="sidebar"><h1>Sidebar</h1><p>I have to nothing to add here.</p></div>');
+document.write(' <div class="sidebar"><h1>Sidebar</h1><p>I have nothing to add here.</p></div>');
