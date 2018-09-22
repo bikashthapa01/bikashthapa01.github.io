486e5dc6b01cbc9b82262e020b087f062736d9d7
Hodor <thapabikash48@gmail.com>
2018-09-22 08:40:17 +0530

    changes header.js

--- blog/js/header.js
+++ blog/js/header.js
@@ -1,10 +1,7 @@
 document.write(
 
   '<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width, initial-scale=1"><title>Blog of Programmer | Bikash Thapa</title><link rel="stylesheet" type="text/css" href="css/style.css"><link href="https://fonts.googleapis.com/css?family=Nunito:400,700" rel="stylesheet"></head><body>'
-
-  );
-
-document.write('<div class="wrapper">'+
++'<div class="wrapper">'+
     '<header class="site-header">'+
         '<div class="site-branding">'+
             '<p><a href=".">Programmers Blog </a><small>By Bikash Thapa</small></p> '+ 
@@ -13,4 +10,5 @@ document.write('<div class="wrapper">'+
         '<div class="site-menu">'+
         '</div>'+
     '</header>'+
-    '<div class="site-content">');
+    '<div class="site-content">'
+  );
